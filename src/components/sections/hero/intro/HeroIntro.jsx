05e256d37cc84215409e4f7f8e0167d618d94474
solovyovsdom <joelborofsky@mac.com>
2023-12-06 16:01:28 -0600

    Add small decorative icons to HeroIntro and HeroPicture components

--- src/components/sections/hero/intro/HeroIntro.jsx
+++ src/components/sections/hero/intro/HeroIntro.jsx
@@ -1,6 +1,8 @@
 import XLHeading from '../../../headings/XLHeading'
 import LargeContainer from '../../../layout/containers/LargeContainer'
 import styles from '../../../../styles/components/sections/hero/HeroIntro.module.scss'
+import SmallIcon from '../../../icons/SmallIcon'
+import { ReactComponent as NotBad } from '../../../../assets/icons/projects/not-bad.svg'
 
 function HeroIntro() {
   return (
@@ -10,7 +12,11 @@ function HeroIntro() {
         My name is <span className='font-handwriting fs-600'>Joel</span>
       </p>
       <p>I am a...</p>
-      <LargeContainer className='bg2 font-handwriting'>Frontend Developer</LargeContainer>
+      <LargeContainer className='bg2 font-handwriting'><span>Frontend Developer</span>
+      <SmallIcon className='icon2 bg6 fillVibrant3'>
+        <NotBad />
+        </SmallIcon>
+      </LargeContainer>
     </div>
   )
 }
--- src/components/sections/hero/picture/HeroPicture.jsx
+++ src/components/sections/hero/picture/HeroPicture.jsx
@@ -4,6 +4,8 @@ import heroPicture from '../../../../assets/img/profile/joel-kid-1.jpg'
 import JPGImage from '../../../images/JPGImage'
 import { ReactComponent as AstronautIcon } from '../../../../assets/icons/navigation/astronaut.svg'
 import { ReactComponent as SunflowerIcon } from '../../../../assets/icons/navigation/sunflower.svg'
+import { ReactComponent as SmilingSun } from '../../../../assets/icons/decorative/smiling-sun.svg'
+import { ReactComponent as Beach } from '../../../../assets/icons/decorative/beach.svg'
 import SmallIcon from '../../../icons/SmallIcon'
 
 function HeroPicture() {
@@ -14,11 +16,17 @@ function HeroPicture() {
           src={heroPicture}
           alt='Joel Borofsky around the age of 7 or 8 in a school photo'
         />
-        <SmallIcon className='icon1 bg6 fill5'>
+        <SmallIcon className='icon1 bg6 fillVibrant2'>
           <AstronautIcon />
         </SmallIcon>
-        <SmallIcon className='icon2 bg4 fill1'>
+        <SmallIcon className='icon2 bg4 fillVibrant1'>
           <SunflowerIcon />
+        </SmallIcon>
+        <SmallIcon className='icon3 bg1 fillVibrant3'>
+          <SmilingSun />
+        </SmallIcon>
+        <SmallIcon className='icon4 bg6 fillVibrant5'>
+          <Beach />
         </SmallIcon>
       </ImageContainer>
     </div>
