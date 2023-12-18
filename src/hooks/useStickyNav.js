43af3c5808df6dbab8f931710b4fb43fff328991
solovyovsdom <joelborofsky@mac.com>
2023-12-18 13:00:05 -0600

    Refactor sticky navigation hook

--- src/hooks/useStickyNav.js
+++ src/hooks/useStickyNav.js
@@ -1,34 +1,3 @@
-// import { useState, useEffect, useRef } from 'react'
-
-// export function useStickyNav() {
-//   const [isSticky, setIsSticky] = useState(false)
-//   const navbarRef = useRef(null)
-//   const titleRef = useRef(null)
-
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       const navbar = navbarRef.current
-//       const title = titleRef.current
-
-//       if (!navbar || !title) return
-
-//       const navPos = navbar.getBoundingClientRect().top
-//       const scrollPos = window.scrollY
-
-//       if (scrollPos > navPos) {
-//         setIsSticky(true)
-//       } else {
-//         setIsSticky(false)
-//       }
-//     }
-
-//     window.addEventListener('scroll', handleScroll)
-//     return () => window.removeEventListener('scroll', handleScroll)
-//   }, [])
-
-//   return { isSticky, navbarRef, titleRef }
-// }
-
 
 import { useState, useEffect, useRef } from 'react'
 
