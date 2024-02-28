4e587a11d218f6b0c5154c4ca9b09487e04039c0
solovyovsdom <joelborofsky@mac.com>
2024-02-28 17:14:19 -0600

    Refactor ProjectCard component to use destructured props

--- src/components/sections/projects/project-layout/project-card/ProjectCard.jsx
+++ src/components/sections/projects/project-layout/project-card/ProjectCard.jsx
@@ -1,24 +1,19 @@
-import { getRandomBgClass } from '../getRandomBgClass'
-import styles from './ProjectCard.module.scss'
 import ProjectIcons from './project-icons/ProjectIcons'
 import LeftIndents from './project-indents/LeftIndents'
 import RightIndents from './project-indents/RightIndents'
 import ProjectLinks from './project-links/ProjectLinks'
 import ProjectTitle from './project-title/ProjectTitle'
+import styles from './ProjectCard.module.scss'
 
 export default function ProjectCard({
-  id,
   title,
-  description,
-  stack,
-  image,
-  github,
-  demo,
+  gitHubHref,
+  gitHubTitle,
+  liveSiteHref,
+  liveSiteTitle,
   className,
 }) {
   const cardClasses = `${styles.projectCard} ${className} || ''`
-  // const linkBgClass = getRandomBgClass(['bgDark', 'bgBlack'])
-  // const linkBgClass2 = getRandomBgClass(['bgDark', 'bgBlack'])
 
   return (
     <div className={cardClasses}>
@@ -30,43 +25,13 @@ export default function ProjectCard({
       </div>
       <div className={styles.bottomHalf}>
         <ProjectLinks
-          gitHubHref='#'
-          gitHubTitle="This Site's Repository!"
-          liveSiteHref='#'
-          liveSiteTitle="This Site's Live Site!"
+          gitHubHref={gitHubHref}
+          gitHubTitle={gitHubTitle}
+          liveSiteHref={liveSiteHref}
+          liveSiteTitle={liveSiteTitle}
         />
         <ProjectIcons />
       </div>
     </div>
-    // <div className={cardClasses} key={id}>
-    //   <div className={styles.cardImage}>
-    //     <img src={image} alt={title} />
-    //     <h4>{title}</h4>
-    //   </div>
-    //   <ul className={styles.cardStack}>
-    //     {stack.map((tech, index) => (
-    //       <li key={index}>{tech}</li>
-    //     ))}
-    //   </ul>
-    //   <div className={styles.cardLinks}>
-    //     <a
-    //       className={linkBgClass}
-    //       href={github}
-    //       target='_blank'
-    //       rel='noopener noreferrer'
-    //     >
-    //       Github
-    //     </a>
-    //     <a
-    //       className={linkBgClass2}
-    //       href={demo}
-    //       target='_blank'
-    //       rel='noopener noreferrer'
-    //     >
-    //       Demo
-    //     </a>
-    //   </div>
-    //   <p className={styles.cardDesc}>{description}</p>
-    // </div>
   )
 }
