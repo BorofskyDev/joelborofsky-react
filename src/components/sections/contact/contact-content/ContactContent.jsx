58690a384bd5b619ccf1bb55038b5466a2a858be
solovyovsdom <joelborofsky@mac.com>
2024-02-29 20:23:05 -0600

    Update contact section styles and add MediumIcon

--- src/components/sections/contact/contact-content/ContactContent.jsx
+++ src/components/sections/contact/contact-content/ContactContent.jsx
@@ -1,6 +1,8 @@
 import SectionTitle from '../../../headings/section-title/SectionTitle'
 import MainHeading from '../../../headings/xl-heading/MainHeading'
 import LargeContainer from '../../../layout/containers/large-container/LargeContainer'
+import MediumIcon from '../../../icons/medium-icon/MediumIcon'
+import { icons } from '../../../icons/icons'
 import styles from './ContactContent.module.scss'
 
 function ContactContent() {
@@ -8,7 +10,15 @@ function ContactContent() {
     <div className={`${styles.contactContent} bg-violet`}>
       <SectionTitle className='bg-green'>Contact</SectionTitle>
       <MainHeading>Hello Again!</MainHeading>
-      <LargeContainer className='bg-yellow'>Let's work together</LargeContainer>
+      <LargeContainer className='bg-yellow'>
+        <span>Let's work together!</span>
+        <MediumIcon
+          icon={icons.handshake.path}
+          viewBox={icons.handshake.viewBox}
+          xmlns={icons.handshake.xmlns}
+          className='bg-vibrant-purple fill-blue top-right'
+        />
+      </LargeContainer>
       <p>
         I'm open for consulting, freelance work, or employment. Feel free to
         reach out!
