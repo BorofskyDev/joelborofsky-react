71c6163c3452c5be6addcb621784780a74c9fc92
solovyovsdom <joelborofsky@mac.com>
2024-07-13 17:44:50 -0500

    feat: Add second project card to Projects section

--- src/components/sections/projects/Projects.jsx
+++ src/components/sections/projects/Projects.jsx
@@ -2,6 +2,7 @@ import SectionTitle from '../../headings/section-title/SectionTitle'
 import ProjectCard from './project-card/ProjectCard'
 import { icons } from '../../icons/icons'
 import firstProjectImg from '../../../assets/img/projects/main-site.jpg'
+import secondProjectImg from '../../../assets/img/projects/second-project-image.png'
 import styles from './Projects.module.scss'
 
 export default function Projects({ className }) {
@@ -34,8 +35,29 @@ export default function Projects({ className }) {
           viewBox4={icons.cat.viewBox}
           iconFill='fill-vibrant-blue'
         />
-       
-       
+        <ProjectCard
+          className='bg-vibrant-red'
+          title='Client Profile Build'
+          src={secondProjectImg}
+          alt='profile site built for data analyst'
+          gitHubHref='https://github.com/BorofskyDev/developer-profile'
+          gitHubTitle='Rebekah Fowler'
+          liveSiteHref='https://rebekahfowler.com'
+          liveSiteTitle='This Site (you are already here)!'
+          techIcon1={icons.nextjs.path}
+          xmlns1={icons.nextjs.xmlns}
+          viewBox1={icons.nextjs.viewBox}
+          techIcon2={icons.sass.path}
+          xmlns2={icons.sass.xmlns}
+          viewBox2={icons.sass.viewBox}
+          techIcon3={icons.framerMotion.path}
+          xmlns3={icons.framerMotion.xmlns}
+          viewBox3={icons.framerMotion.viewBox}
+          decorativeIcon={icons.fireHeart.path}
+          xmlns4={icons.fireHeart.xmlns}
+          viewBox4={icons.fireHeart.viewBox}
+          iconFill='fill-vibrant-blue'
+        />
       </div>
     </section>
   )
